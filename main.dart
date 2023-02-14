5ead4cb9329ed6e5f90bf24f39b05937b641eb70
trizahildah <122311866+trizahildah@users.noreply.github.com>
2023-02-14 09:20:33 +0000

    Initial commit

---
+++ main.dart
@@ -0,0 +1,41 @@
+int age = 45;
+double students = 1/78;
+String cohort ="cohort 3 January";
+String name ="Hildah Trizah Angehi";
+main() {
+  print(age);
+  print(students);
+  print(cohort);
+  print(name);
+  var countries = ["Kenya","Nigeria", "Tanzania"];
+     print(countries);
+  var student ={'name':'Tom','country':'Kenya'};
+  print(student);
+  var heart_symbol = '\u2665';  
+    var laugh_symbol = '\u{1f600}';  
+    print(heart_symbol);  
+    print(laugh_symbol); 
+  var names = 'Voyager I';
+  print(names);
+var year = 1977;
+  print(year);
+var antennaDiameter = 3.7;
+var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+print(antennaDiameter);
+  print(flybyObjects);
+  var image = {
+  'tags': ['saturn'],
+  'url': 'https://thenational-the-national-prod.cdn.arcpublishing.com/resizer/Dz-r8HpDo7fHth1WB375xmywwrY=/800x0/filters:format(jpg):quality(70)/cloudfront-eu-central-1.images.arcpublishing.com/thenational/5THRTVY2MRHVJPWEYQ4JXHHRFE.jpg'
+  };
+if ( 2 > 4 );
+  {
+    print( '2 is greater than 4');
+    } 
+  'else';
+  {
+    print( '2 is not greater than 4');
+    };
+  
+  
+   
+}
